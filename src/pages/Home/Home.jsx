6930153382f512335a4cedaa4184e42238f0473c
alialaba <alialaba079@gmail.com>
2023-10-28 15:25:17 -0400

    style: style stats section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,9 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 import "./Home.css";
 import InlineInputBtn from "../../components/InlineInputBtn";
 import AccordionItem from "../../components/Accordion";
-import ftImg from "../../assets/tes.png"
+// import ftImg from "../../assets/tes.png"
 
 export default function Home(props){
 
@@ -12,6 +12,11 @@ export default function Home(props){
     const handleToggle =(index)=>{
       setClicked(index === clicked ? null : index)
     }
+
+    useEffect(() => {
+        // Add an effect to open the first item by default
+        setClicked(0);
+      }, []);
 
 
     return(<>
@@ -34,17 +39,51 @@ export default function Home(props){
             </div>
         </section>
 
+        <section  className="section__stats">
+            <div className="container">
+                <div className="stats stack">
+                   <h2 className="stats__title">One Stop. <br></br>Four<span> Possibilities.</span></h2>
+                   <ul className="stats__row">
+                    <li className="stats__col">
+                        <h3 className="stats__value">3M</h3>
+                        <span className="stats__caption">Active users</span>
+                    </li>
+                    <li className="stats__col">
+                        <h3 className="stats__value">30M</h3>
+                        <span className="stats__caption">Links & QR codes created</span>
+                    </li>
+                    <li className="stats__col">
+                        <h3 className="stats__value">4B</h3>
+                        <span className="stats__caption">Clicked & Scanned connections</span>
+                    </li>
+                    <li className="stats__col">
+                        <h3 className="stats__value">100K</h3>
+                        <span className="stats__caption">App Integrations</span>
+                    </li>
+
+                   </ul>
+                </div>
+
+            </div>
+        </section>
+
+
         <section className="section__features">
          <div className="container">
             <h2 className="subtitle">Get the most out of every link you share</h2>
+            
             <div className="features">
 
             <article className="features__img">
-                   <img src={ftImg} alt="feature imag"/>
+                 
+                   {props.featuresData.map((item,index)=>(
+                    <img key={item.id} style={{display: clicked === index ? "block" : "none"}}  src={item.featureImg} alt="feature imag"/>
+                    
+                   ))}
 
             </article>
-                  
-                <article className="features__content">
+                   
+                 <article className="features__content">
                     <h3 className="whyus__title">Why Us?</h3>
                     <p>shortlyurl provides advanced features that let businesses get all the benefits of link shortening.</p>
                 
@@ -57,10 +96,6 @@ export default function Home(props){
                 ))}
                 
                 </article>
-
-                
-
-          
 
             </div>
 
--- src/utils/featureData.js
+++ src/utils/featureData.js
@@ -1,33 +1,31 @@
-const questions = [
+const features = [
     {
       id: 1,
-      title: 'Do I have to allow the use of cookies?',
+      title: 'Detailed Statistics',
       info:
-        'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
+        'Monitor the performance of each brief URL and domain, obtaining comprehensive analytics such as operating system, geographic location, web browser, and up-to-the-minute clickstream data.',
+      featureImg: "https://images.unsplash.com/photo-1508768787810-6adc1f613514?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e27f6661df21ed17ab5355b28af8df4e&auto=format&fit=crop&w=1350&q=80"
     },
     {
       id: 2,
-      title: 'How do I change my My Page password?',
+      title: 'Teams with Roles',
       info:
-        'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
-    },
+        'Establish teams for the purpose of shortening and controlling URLs alongside your coworkers. Assign three distinct roles: Administrator, User, and Read-only.',
+        featureImg: "https://images.unsplash.com/photo-1510942201312-84e7962f6dbb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=da4ca7a78004349f1b63f257e50e4360&auto=format&fit=crop&w=1050&q=80"
+      },
     {
       id: 3,
-      title: 'What is BankID?',
+      title: 'API',
       info:
-        'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
-    },
+        'Utilize a no-cost API designed for developers to shorten, analyze, and oversee URLs. Integrate shortening capabilities into your current application or develop a brand-new one.',
+        featureImg: "https://images.unsplash.com/photo-1486899430790-61dbf6f6d98b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8ecdee5d1b3ed78ff16053b0227874a2&auto=format&fit=crop&w=1002&q=80"
+      },
     {
       id: 4,
-      title: 'Whose birth number can I use?',
+      title: 'Multiple custom domains',
       info:
-        'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
-    },
-    {
-      id: 5,
-      title: 'When do I recieve a password ordered by letter?',
-      info:
-        'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
-    },
+        'Utilize multiple branded domains within a single account. Implement unique configurations for each domain, and monitor the audience separately for each project.',
+      featureImg: "https://images.unsplash.com/photo-1510942201312-84e7962f6dbb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=da4ca7a78004349f1b63f257e50e4360&auto=format&fit=crop&w=1050&q=80"
+      }
   ]
-  export default questions
+  export default features
