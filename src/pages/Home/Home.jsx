fd3882a17ad9c8ac05aeb1d4e6c443c2ed6999fa
alialaba <alialaba079@gmail.com>
2023-11-16 14:20:42 -0500

    installformik/yup package

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import "./Home.css";
 import InlineInputBtn from "../../components/InlineInputBtn";
@@ -6,153 +6,150 @@ import AccordionItem from "../../components/Accordion";
 import ActionCard from "../../components/ActionCard";
 // import ftImg from "../../assets/tes.png"
 
-export default function Home(props){
+export default function Home(props) {
+  //Track the active item.
+  const [activeItem, setActiveItem] = useState(0);
 
-    //Track the active item.
-    const [activeItem, setActiveItem] = useState(0);
+  //Keep track of which FAQ item is open
+  const [openFAQ, setOpenFAQ] = useState(null);
 
-    //Keep track of which FAQ item is open
-    const [openFAQ, setOpenFAQ] = useState(null);
-
-    //Function to toggle the active item.
-    const handleToggle = (index)=>{
-        if(index === activeItem){
-         // Clicking on the open item should not close it.
-          return;
-        }
-        setActiveItem(index)
-        console.log(index);
+  //Function to toggle the active item.
+  const handleToggle = (index) => {
+    if (index === activeItem) {
+      // Clicking on the open item should not close it.
+      return;
     }
+    setActiveItem(index);
+    console.log(index);
+  };
 
-    //Function to toggle faq for a specific FAQ item
-    const handleFaqToggle = (index)=>{
-          setOpenFAQ(openFAQ === index ? null : index )
-    }
+  //Function to toggle faq for a specific FAQ item
+  const handleFaqToggle = (index) => {
+    setOpenFAQ(openFAQ === index ? null : index);
+  };
 
+  return (
+    <>
+      <section className="section__hero">
+        <div className="container">
+          <div className="hero stack center">
+            <h1 className="hero__title">
+              Shorten <span>Links</span> with Ease.
+            </h1>
 
-    
-
-    return(<>
-       <section className="section__hero">
-            <div className="container">
-                <div className="hero stack center">
-                   
-                    <h1 className="hero__title">Shorten <span >Links</span> with Ease.</h1>
-                   
-                    <p className="hero__text">Shorten, personalize, and share fully branded short URLs.</p>
-                    <div><InlineInputBtn/></div>
-                    <ul className="hero__list">
-                        <li className="hero__item">
-                        <span className="hero__icon">
-                        <i class="fa-solid fa-link"></i>
-                        </span>
-                            <span>Short Links</span>
-                        </li>
-                        <li className="hero__item">
-                         <span className="hero__icon">
-                         <i class="fa-solid fa-qrcode"></i>
-                        </span>
-                            <span>QR Codes</span>
-                        </li>
-
-                    </ul>
-                </div>
+            <p className="hero__text">
+              Shorten, personalize, and share fully branded short URLs.
+            </p>
+            <div>
+              <InlineInputBtn />
             </div>
-        </section>
+            <ul className="hero__list">
+              <li className="hero__item">
+                <span className="hero__icon">
+                  <i class="fa-solid fa-link"></i>
+                </span>
+                <span>Short Links</span>
+              </li>
+              <li className="hero__item">
+                <span className="hero__icon">
+                  <i class="fa-solid fa-qrcode"></i>
+                </span>
+                <span>QR Codes</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
 
-        <section  className="section__stats">
-            <div className="container">
-                <div className="stats stack">
-                    <div className="">
-                   <h4 className="stats__title">One Stop. <br></br>Four<span> Possibilities.</span></h4>
-                   </div>
-                   <ul className="stats__row">
-                    <li className="stats__col">
-                        <h4 className="stats__value">3M</h4>
-                        <span className="stats__caption">Active users</span>
-                    </li>
-                    <li className="stats__col">
-                        <h4 className="stats__value">30M</h4>
-                        <span className="stats__caption">Links & QR codes created</span>
-                    </li>
-                    <li className="stats__col">
-                        <h4 className="stats__value">4B</h4>
-                        <span className="stats__caption">activeItem & Scanned connections</span>
-                    </li>
-                    <li className="stats__col">
-                        <h4 className="stats__value">100K</h4>
-                        <span className="stats__caption">App Integrations</span>
-                    </li>
-
-                   </ul>
-                </div>
-
+      <section className="section__stats">
+        <div className="container">
+          <div className="stats stack">
+            <div className="">
+              <h4 className="stats__title">
+                One Stop. <br></br>Four<span> Possibilities.</span>
+              </h4>
             </div>
-        </section>
+            <ul className="stats__row">
+              <li className="stats__col">
+                <h4 className="stats__value">3M</h4>
+                <span className="stats__caption">Active users</span>
+              </li>
+              <li className="stats__col">
+                <h4 className="stats__value">30M</h4>
+                <span className="stats__caption">Links & QR codes created</span>
+              </li>
+              <li className="stats__col">
+                <h4 className="stats__value">4B</h4>
+                <span className="stats__caption">
+                  activeItem & Scanned connections
+                </span>
+              </li>
+              <li className="stats__col">
+                <h4 className="stats__value">100K</h4>
+                <span className="stats__caption">App Integrations</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
 
+      <section className="section__features">
+        <div className="container">
+          <h2 className="subheading">
+            Get the most out of every link you share
+          </h2>
 
-        <section className="section__features">
-         <div className="container">
-            <h2 className="subheading">Get the most out of every link you share</h2>
-            
-            <div className="features stack">
-
+          <div className="features stack">
             <article className="features__img">
-                 
-                   {props.featuresData.map((item,index)=>(
-                    <img key={item.id} style={{display: activeItem === index ? "block" : "none"}}  src={item.featureImg} alt="feature imag"/>
-                    
-                   ))}
-
-            </article>
-                   
-                 <article className="features__content stack">
-                    <h3 className="features__title">See what's inside</h3>
-                    <p className="features__text">shortlyurl provides advanced features that let businesses get all the benefits of link shortening.</p>
-                
-                {props.featuresData.map((item, index)=>(
-                   <AccordionItem
-                    key={item.id} item={item} 
-                    onToggle={()=> handleToggle(index)}
-                    active={activeItem === index}
-                     />
-                ))}
-                
-                </article>
-
-            </div>
-
-
-                <ActionCard/>
-
-
-         </div>
-
-        </section>
-        
-
- 
-        {/* FAQ SECTION */}
-        <section className="faq-section">
-            <div className="container">
-             
-              <div className="faq stack">
-              <h2 className="subheading">Frequently Asked Questions</h2>
-
-              {props.faqData.map((item, index)=>(
-                <AccordionItem 
-                key={item.id} item={item}
-                open={openFAQ === index}
-                onToggle={()=> handleFaqToggle(index)}
-                
+              {props.featuresData.map((item, index) => (
+                <img
+                  key={item.id}
+                  style={{ display: activeItem === index ? "block" : "none" }}
+                  src={item.featureImg}
+                  alt="feature imag"
                 />
               ))}
+            </article>
 
-              </div>
+            <article className="features__content stack">
+              <h3 className="features__title">See what's inside</h3>
+              <p className="features__text">
+                shortlyurl provides advanced features that let businesses get
+                all the benefits of link shortening.
+              </p>
 
+              {props.featuresData.map((item, index) => (
+                <AccordionItem
+                  key={item.id}
+                  item={item}
+                  onToggle={() => handleToggle(index)}
+                  active={activeItem === index}
+                />
+              ))}
+            </article>
+          </div>
 
-            </div>
-        </section>
-        
-    </>)
+          <ActionCard />
+        </div>
+      </section>
+
+      {/* FAQ SECTION */}
+      <section className="faq-section">
+        <div className="container">
+          <div className="faq stack">
+            <h2 className="subheading">Frequently Asked Questions</h2>
+
+            {props.faqData.map((item, index) => (
+              <AccordionItem
+                key={item.id}
+                item={item}
+                open={openFAQ === index}
+                onToggle={() => handleFaqToggle(index)}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
--- src/pages/LoginSignup/LoginSignup.jsx
+++ src/pages/LoginSignup/LoginSignup.jsx
@@ -1,49 +1,143 @@
-import React, {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button";
-import "./LoginSignup.css"
-export default function SignUp(){
+import "./LoginSignup.css";
+export default function SignUp() {
+  // let navigate = useNavigate();
+  // control login/signup state changes
+  const [action, setAction] = useState("Login");
 
-    // let navigate = useNavigate();
-    const [action, setAction] = useState("Login")
+  const initialValues = { fullname: "", email: "", password: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-    return(
-        <section>
-           <div className="container">
-            <div className="credential stack">
-            <div className="credential__header">
-                <Button text="Login" onClick={()=> setAction("Login")} className={` ${action === "Login" ? "credential__btn" : "credential__btn gray"}`}/>
-              
-                <Button text="Sign Up" onClick={()=> setAction("Sign Up")} className={`${action === "Sign Up" ? "credential__btn" : "credential__btn gray"}`}/>
+  useEffect(() => {
+    if (Object.keys(formErrors).length === 0 && isSubmitting) {
+      submitForm();
+    }
+  }, [formErrors]);
+  const submitForm = () => {
+    console.log(formValues);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmitting(true);
+  };
+
+  const validate = (values) => {
+    let errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+
+     if(!values.fullname){
+      errors.fullname ="Input cannot be blank";
+     }
+
+    if (!values.email) {
+      errors.email = "Input cannot be blank";
+    } else if (!regex.test(values.email)) {
+      errors.email = "Invalid email format";
+    }
+
+    if (!values.password) {
+      errors.password = "Input cannot be blank";
+    } else if (values.password.length < 5) {
+      errors.password = "Password must be more than 5 characters";
+    }
+
+    return errors;
+  };
+
+  return (
+    <section>
+      <div className="container">
+        <div className="credential stack">
+          <div className="credential__header">
+            <Button
+              text="Login"
+              onClick={() => setAction("Login")}
+              className={` ${
+                action === "Login" ? "credential__btn" : "credential__btn gray"
+              }`}
+            />
+
+            <Button
+              text="Sign Up"
+              onClick={() => setAction("Sign Up")}
+              className={`${
+                action === "Sign Up"
+                  ? "credential__btn"
+                  : "credential__btn gray"
+              }`}
+            />
+          </div>
+
+          <form className="credential__form" onSubmit={handleSubmit} noValidate>
+            {action === "Login" ? (
+              <></>
+            ) : (
+              <div className="form-col">
+              <div className="form-input">
+                {/* <label htmlFor="fullname">Fullname</label> */}
+                <i className="fa-regular fa-user"></i>
+                <input
+                  type="text"
+                  name="fullname"
+                  id="fullname"
+                  placeholder="Fullname"
+                  value={formValues.fullname}
+                  onChange={handleChange}
+                />
+              </div>
+              {formErrors.fullname && <span className="error">{formErrors.fullname}</span>}
+              </div>
+            )}
+             <div className="form-col">
+            <div className="form-input">
+              {/* <label htmlFor="email">Email</label> */}
+              <i className="fa-regular fa-envelope"></i>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={formValues.email}
+                onChange={handleChange}
+                placeholder="Email"
+              />
             </div>
-
-            <div className="credential__form">
-
-                {action === "Login" ? 
-                <></> : 
-                <div className="form-col">
-               {/* <label htmlFor="fullname">Fullname</label> */}
-               <i class="fa-regular fa-user"></i>
-               <input type="text" name="name" placeholder="Fullname"/>
-               </div>
-                 }
-               
-               <div className="form-col">
-               {/* <label htmlFor="email">Email</label> */}
-               <i class="fa-regular fa-envelope"></i>
-               <input type="email" placeholder="Email"/>
-               </div>
-               <div className="form-col">
-               <i class="fa-solid fa-lock"></i>
-               {/* <label htmlFor="password">Password</label> */}
-               <input type="text" placeholder="Password"/>
-               <i class="fa-regular fa-eye"></i>
-               </div>
-               <Button className="credential__btn credential__submit" text={`${action === "Login" ? "Login" :"Sign Up"}`}/>
+            {formErrors.email && <span className="error">{formErrors.email}</span>}
             </div>
-            
-            </div> 
+            <div className="form-col">
+            <div className="form-input">
+              <i className="fa-solid fa-lock"></i>
+              {/* <label htmlFor="password">Password</label> */}
+              <input
+                type="text"
+                name="password"
+                id="password"
+                value={formValues.password}
+                onChange={handleChange}
+                placeholder="Password"
+              />
+              <i className="fa-regular fa-eye"></i>
             </div>
-        </section>
-    );
+            {formErrors.password && <span className="error" >{formErrors.password}</span>}
+            </div>
+            <Button
+              className="credential__btn credential__submit"
+              text={`${action === "Login" ? "Login" : "Sign Up"}`}
+            />
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
